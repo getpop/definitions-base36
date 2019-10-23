147a2dd932c38c05889cc51a2a9fb4a0f3f8b96e
Leo <losoviz@gmail.com>
2019-10-23 17:27:41 +0800

    Migrated class

---
+++ src/Definitions/DefinitionResolver.php
@@ -0,0 +1,56 @@
+<?php
+namespace PoP\Base36Definitions\Definitions;
+use PoP\Definitions\Definitions\AbstractDefinitionResolver;
+
+class DefinitionResolver extends AbstractDefinitionResolver
+{
+    private $definition_counters = [];
+
+    // Allow PoP Persistent Definitions to set a different value
+    public function setPersistedData($persisted_data): void
+    {
+        // The first time there will be no persisted data
+        if ($counters = $persisted_data['counters']) {
+            $this->definition_counters = $counters;
+        }
+    }
+    public function getDataToPersist(): array
+    {
+        return array(
+            'counters' => $this->definition_counters,
+        );
+    }
+
+    public function getDefinition($name, $group): string
+    {
+        // Counter: cannot start with a number, or the id will get confused
+        // First number is 10, that is "a" in base 36
+        if (!$this->definition_counters[$group]) {
+            $this->definition_counters[$group] = 10;
+        }
+
+        // Convert the number to base 36 to save chars
+        $counter = base_convert($this->definition_counters[$group], 10, 36);
+
+        // Increase the counter by 1.
+        $this->definition_counters[$group] = $this->definition_counters[$group]+1;
+
+        // If we reach a number whose base 36 conversion starts with a number, and not a letter, then skip
+        if ($this->definition_counters[$group] == 36) {
+            // 36 in base 10 = 10 in base 36
+            // 360 in base 10 = a0 in base 36
+            $this->definition_counters[$group] = 360;
+        } elseif ($this->definition_counters[$group] == 1296) {
+            // 1296 in base 10 = 100 in base 36
+            // 12960 in base 10 = a00 in base 36
+            $this->definition_counters[$group] = 12960;
+        } elseif ($this->definition_counters[$group] == 46656) {
+            // 46656 in base 10 = 1000 in base 36
+            // 466560 in base 10 = a000 in base 36
+            $this->definition_counters[$group] = 466560;
+        }
+
+        // That's it, return the result
+        return $counter;
+    }
+}
